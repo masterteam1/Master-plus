c2ba6b0bce3cefc5c214327adc0ac00d39319acf
masterteam1 <zozo.prince7@gmail.com>
2017-02-13 23:10:06 +0300

    MASTER TEAM

---
+++ plugins/hban.lua
@@ -0,0 +1,45 @@
+--MASTER TEAM
+do 
+
+local function master(msg,matches) 
+
+local reply_id = msg['id'] 
+if is_momod(msg) and matches[1]== 'hban' then 
+  local banhelp = [[💠 HBAN 💠
+    🌐 اوامر الطرد والحظر 🌐
+🔸➖🔸➖🔸➖🔸➖🔸
+💡/kick : طرد عضو
+💡/ban : حظر عضو
+💡/unban : فتح الحظر 
+💡/blist : قائمة المحظورين
+🔹➖🔹➖🔹➖🔹➖🔹
+✔️BADWORDS | الكلمات السيئه
+🎖/filter + كلمه : اضافة كلمه ممنوعه
+🎖/unfilter + كلمه : حذف كلمه ممنوعه
+🎖/clean filtelist : حذف الكلمات الممنوعه
+🔹➖🔹➖🔹➖🔹➖🔹
+✔️SILENTS | كتم الاعضاء
+🎭/silent : كتم عضو
+🎭/unsilent : الغاء كتم العضو
+🎭/clean silentlist : حذف قائمة المكتومين
+🔸➖🔸➖🔸➖🔸➖🔸
+🌐M A S T E R®💠BOT💠
+Copyright © 2016.MASTER™]]
+reply_msg(reply_id, banhelp, ok_cb, false) 
+end 
+
+local reply_id = msg['id'] 
+if not is_momod(msg) then 
+local banhelp = "للمشرفين فقط 🖕🏿😎" 
+reply_msg(reply_id, banhelp, ok_cb, false) 
+end 
+
+end 
+return { 
+patterns ={ 
+  "^[!#/](hban)$", 
+}, 
+run = master
+} 
+end 
+--  MASTER TEAM -_-
